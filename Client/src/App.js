59d1defd1ff95b686a410574bcbf5ef69a5bf491
Hirdeshgupta <hirdeshgupta16@gmail.com>
2021-09-20 00:38:05 +0530

    Folder Structure

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,18 @@
-
+import ChannelContainer from "./Components/ChannelConatiner";
+import ChannelListSContainer from "./Components/ChannelListsContainer";
+import {StreamChat} from "stream-chat";
+import {Chat} from "stream-chat-react";
+import Cookie from "universal-cookie";
+import "./App.css";
+const client = StreamChat.getInstance(process.env.STREAM_API_KEY);
 function App() {
   return (
-    <h1>Welcome Back</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelContainer />
+        <ChannelListSContainer/>
+      </Chat>
+    </div>
   );
 }
 
---
+++ Client/src/Components/ChannelListsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelListsContainer = () => {
+    return (
+        <div>
+            ChannelListsContainer
+        </div>
+    )
+}
+
+export default ChannelListsContainer
